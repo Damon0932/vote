44ec5ac96c8fe85fadbe19f3d5b9b957146e9d9b
Damon <whliujun@hotmail.com>
2016-11-11 15:02:03 +0800

    fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,26 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 /**
- * Class VoteController
+ * Class AdminController
  * @package App\Http\Controllers
  */
-class VoteController extends Controller
+class AdminController extends Controller
 {
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index()
-    {
-        return view('vote.index');
-    }
-
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function vote(Request $request)
-    {
-        return view('vote.vote');
-    }
-
-
 }
