167e428aa5ad78e73d66246d46b0b2dc02d14c5a
Damon <whliujun@hotmail.com>
2016-11-14 20:06:08 +0800

    fix

--- database/migrations/2016_11_11_000000_create_votes_table.php
+++ database/migrations/2016_11_11_000000_create_votes_table.php
@@ -16,7 +16,7 @@ class CreateVotesTable extends Migration
             $table->increments('id');
             $table->string('img_url');
             $table->string('name');
-            $table->string('question');
+            $table->string('category');
             $table->timestamps();
         });
 
