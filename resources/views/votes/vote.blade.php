688d2af8c6bf74973943e354d64f4397f3c4143d
Damon <whliujun@hotmail.com>
2016-11-16 18:14:04 +0800

    fix

--- resources/views/votes/vote.blade.php
+++ resources/views/votes/vote.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vote</title>
-    <!-- <link rel="shortcut icon" href="/favicon.ico"> -->
+    <title>投票</title>
     <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
     <link rel="stylesheet" href="css/vendor/font-awesome.min.css">
     <link rel="stylesheet" href="css/vendor/swiper-3.3.0.min.css">
@@ -19,7 +18,7 @@
     <div class="container">
         <div class="media">
             <a class="media-left">
-                <img src="img/logo_150.png" alt="">
+                <img src="img/logo.png" alt="">
             </a>
 
             <div class="media-body">
@@ -31,10 +30,10 @@
     <div class="container">
         <div class="word">
             <div>
-                You have rated
+                你已经投了
                 <b>@{{ vote_count }}</b>
                 <b>/</b>
-                <b>@{{ vote.length }}</b> styles.
+                <b>@{{ vote.length }}</b> 票.
             </div>
         </div>
         <button type="button" @click='submit_vote' class="button button-caution button-uppercase" id='submit_vote'>
@@ -43,16 +42,7 @@
 </div>
 <div class="container" style="min-height: 800px;">
     <ol class="breadcrumb">
-        <li><a href="/">Home</a></li>
-        <li><a href="#">Vote</a></li>
     </ol>
-    <div class="col-lg-12 light-block text-center">
-        <h2 style="margin: 0;color: #f28794;font-weight: 300;">Gothic Chic</h2>
-        <hr>
-        <p><span class="text-danger">*</span> Please rate ALL OUTFITS by clicking the stars before
-            <button class="button-caution" style="padding: 0 4px; border: none;">submit</button>
-        </p>
-    </div>
     <div class="row">
         <div class="col-lg-4 col-md-4" v-for="item in vote" track-by="$index">
             <div class="thumbnail">
@@ -60,11 +50,11 @@
 
                 <div class="caption" v-for="question in item.questions" v-cloak>
                     <p>
-                        <span>Question@{{ $index+1 }}:</span>@{{{ question.question }}}
+                        <span>@{{ $index+1 }}:</span>@{{{ question.question }}}
                     </p>
 
                     <p v-if=" question.type == 'select'">
-                            <span>Answer: <span class="text-danger">*</span>
+                            <span>答案: <span class="text-danger">*</span>
                             <span class="star">
                                 <i class="fa fa-star" :class=" question.answer >= 1 ? 'choose':''" @click="star(question,1)"></i>
                                 <i class="fa fa-star" :class=" question.answer >= 2 ? 'choose':''" @click="star(question,2)"></i>
@@ -82,7 +72,7 @@
                     </p>
 
                     <div class="form-group" style="margin-top: 10px;" v-if=" question.type == 'comment'">
-                        <textarea rows="2" class="form-control" placeholder="Leave a comment. (Optional)"
+                        <textarea rows="2" class="form-control" placeholder=""
                                   v-model="question.answer"></textarea>
                     </div>
                 </div>
@@ -90,37 +80,6 @@
         </div>
     </div>
 </div>
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-sm-12 col-xs-12 footer-block">
-                <h2>About</h2>
-
-                <p>
-                    Company Limited.
-                </p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-xs-12 footer-block">
-                <h2>Shares</h2>
-
-                <p><i class="fa fa-weibo"></i><i class="fa fa-wechat"></i><i class="fa fa-qq"></i><i
-                            class="fa fa-skype"></i></p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-xs-12 footer-block">
-                <h2>Contacts</h2>
-                <address class="margin-bottom-40">
-                    Email: <a href="mailto:123@123.com">123@123.com</a>
-                </address>
-            </div>
-        </div>
-    </div>
-</footer>
-{{-- <div class="copy-info">
-    <div class="container">
-        <p>2016 Copyright © 123@123 Co. Ltd. All Rights Reserved.</p>
-    </div>
-</div> --}}
-
 <script type="text/javascript">
     var data = {!! $products !!}
 </script>
