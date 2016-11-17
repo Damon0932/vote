5d7d0368892adefa1ab9c540473faf6809b3d638
Damon <whliujun@hotmail.com>
2016-11-17 14:39:28 +0800

    fix

--- resources/views/votes/detail-table.blade.php
+++ resources/views/votes/detail-table.blade.php
@@ -36,8 +36,8 @@
 </div>
 
 
-<script type="text/javascript" src="js/vendor/jquery.min.js"></script>
-<script type="text/javascript" src="js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="/js/vendor/bootstrap.min.js"></script>
 </body>
 
 </html>
