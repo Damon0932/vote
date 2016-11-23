e520fe0d4ae6a6f66804d2ddafdc2370596960c5
Damon <whliujun@hotmail.com>
2016-11-23 14:35:14 +0800

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -65,15 +65,11 @@ class Product extends Model
     {
         $questions = [
             [
-                'question' => '如果这件衣服' . rand(1000, 10) . '元一件(套），请问您愿意买这件衣服吗？',
+                'question' => '如果这件衣服' . $this->attributes['price'] . '元一件(套），请问您愿意买这件衣服吗？',
                 'type' => 'select',
                 'answer' => ''
             ], [
                 'question' => '请问您对这款衣服的款式评价如何？',
-                'type' => 'select',
-                'answer' => ''
-            ], [
-                'question' => '请问您对这款衣服的颜色评价如何？',
                 'type' => 'select',
                 'answer' => ''
             ]
@@ -83,27 +79,11 @@ class Product extends Model
                 'question' => '请问您对这款文胸的功能评价如何？',
                 'type' => 'select',
                 'answer' => ''
-            ], [
-                'question' => '对于这款衣服，您觉得哪里最能打动你？（选填）？',
-                'type' => 'comment',
-                'answer' => ''
-            ], [
-                'question' => '对于这款衣服，您举得哪里还不够好？（选填）',
-                'type' => 'comment',
-                'answer' => ''
             ]);
         } else {
             array_push($questions, [
                 'question' => '请问您对这款家居服的面料评价如何？',
                 'type' => 'select',
-                'answer' => ''
-            ], [
-                'question' => '对于这款衣服，您觉得哪里最能打动你？（选填）？',
-                'type' => 'comment',
-                'answer' => ''
-            ], [
-                'question' => '对于这款衣服，您举得哪里还不够好？（选填）',
-                'type' => 'comment',
                 'answer' => ''
             ]);
         }
